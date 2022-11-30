fc4664593f0c2de5f197c7a6a1ced5163a7efb34
Chandra Kiran <kallepallychandrakiran@gmail.com>
2022-11-30 20:12:09 +0530

    Anagram code added

---
+++ Assignment4/src/Anagram.java
@@ -0,0 +1,35 @@
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			
+			String s1 = "Triangle";
+			String s2 = "Integrapp";
+			s1 = s1.toLowerCase();
+			s2 = s2.toLowerCase();
+			char [] ch1 =  s1.toCharArray();
+			char [] ch2 =  s2.toCharArray();
+			int sum1 =0;
+			int sum2 =0;
+			if(s1.length()!=s2.length())
+			{
+		System.out.println("This is not an anagram");
+			System.exit(0);
+			}
+			for(int i=0;i<s1.length();i++)
+			{
+				sum1+=(int)ch1[i];
+				sum2+=(int)ch2[i];
+			}
+				
+			if(sum1==sum2)
+				System.out.println("This is an anagram");
+			else
+				System.out.println("This is not an anagram");
+			
+	}
+
+}
